49eb3266ffce2447ae86bd7cc65292e370626a60
samini <soufianeamini2002@gmail.com>
2024-06-27 19:12:22 +0100

    implemented operator overloads, need to fix templates

--- operand/Operand.hpp
+++ operand/Operand.hpp
@@ -1,5 +1,8 @@
 #include "IOperand.hpp"
+#include "OperandFactory.hpp"
+#include <cmath>
 #include <sstream>
+#include <type_traits>
 #include <utility>
 template <eOperandType E, class T> class Operand : public IOperand {
 private:
@@ -24,11 +27,101 @@ public:
   }
   int getPrecision(void) const { return std::to_underlying(E); }
   eOperandType getType(void) const { return E; }
-  IOperand const *operator+(IOperand const &rhs) const;
-  IOperand const *operator-(IOperand const &rhs) const;
-  IOperand const *operator*(IOperand const &rhs) const;
-  IOperand const *operator/(IOperand const &rhs) const;
-  IOperand const *operator%(IOperand const &rhs) const;
+  IOperand const *operator+(IOperand const &rhs) const {
+    OperandFactory of;
+    if (this->getPrecision() >= rhs.getPrecision()) {
+      std::stringstream ss(rhs.toString());
+      T value;
+
+      ss >> value;
+      ss << (this->value + value);
+
+      std::string result;
+      return of.createOperand(E, result);
+    } else {
+      return rhs + *this;
+    }
+  }
+  IOperand const *operator-(IOperand const &rhs) const {
+    OperandFactory of;
+    if (this->getPrecision() >= rhs.getPrecision()) {
+      std::stringstream ss(rhs.toString());
+      T value;
+
+      ss >> value;
+      ss << (this->value - value);
+
+      std::string result;
+      return of.createOperand(E, result);
+    } else {
+      return rhs - *this;
+    }
+  }
+  IOperand const *operator*(IOperand const &rhs) const {
+    OperandFactory of;
+    if (this->getPrecision() >= rhs.getPrecision()) {
+      std::stringstream ss(rhs.toString());
+      T value;
+
+      ss >> value;
+      ss << (this->value * value);
+
+      std::string result;
+      return of.createOperand(E, result);
+    } else {
+      return rhs * *this;
+    }
+  }
+  IOperand const *operator/(IOperand const &rhs) const {
+    OperandFactory of;
+    if (this->getPrecision() >= rhs.getPrecision()) {
+      std::stringstream ss(rhs.toString());
+      T value;
+
+      ss >> value;
+      ss << (this->value / value);
+
+      std::string result;
+      return of.createOperand(E, result);
+    } else {
+      return rhs / *this;
+    }
+  }
+  template <typename Integer,
+            std::enable_if_t<std::is_integral<Integer>::value, bool> = true>
+  IOperand const *operator%(IOperand const &rhs) const {
+    OperandFactory of;
+    if (this->getPrecision() >= rhs.getPrecision()) {
+      std::stringstream ss(rhs.toString());
+      T value;
+
+      ss >> value;
+      ss << (this->value % value);
+
+      std::string result;
+      return of.createOperand(E, result);
+    } else {
+      return rhs % *this;
+    }
+  }
+  template <
+      typename Floating,
+      std::enable_if_t<std::is_floating_point<Floating>::value, bool> = true>
+  IOperand const *operator%(IOperand const &rhs) const {
+    OperandFactory of;
+    if (this->getPrecision() >= rhs.getPrecision()) {
+      std::stringstream ss(rhs.toString());
+      T value;
+
+      ss >> value;
+      ss << (fmod(this->value, value));
+
+      std::string result;
+      return of.createOperand(E, result);
+    } else {
+      return rhs % *this;
+    }
+  }
   std::string const &toString(void) const { return toStr; }
   ~Operand(void) {}
 };
