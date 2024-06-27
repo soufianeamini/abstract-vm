c7b76f787ee4ffcb7721f73cc844bf8c628ab64c
samini <soufianeamini2002@gmail.com>
2024-06-27 12:59:48 +0100

    class prototype+ comments

--- operand/Operand.cpp
+++ operand/Operand.cpp
@@ -1,5 +1,10 @@
 #include "Operand.hpp"
 #include "IOperand.hpp"
-template <eOperandType E> eOperandType Operand<E>::getType(void) const {
+
+template <eOperandType E, class T>
+eOperandType Operand<E, T>::getType(void) const {
   return E;
 }
+
+template <eOperandType E, class T>
+IOperand const *Operand<E, T>::operator+(IOperand const &rhs) const {}
--- operand/Operand.hpp
+++ operand/Operand.hpp
@@ -1,4 +1,18 @@
 #include "IOperand.hpp"
-template <eOperandType E> class Operand : IOperand {
+template <eOperandType E, class T> class Operand : IOperand {
+  int getPrecision(void) const;
   eOperandType getType(void) const;
+  IOperand const *operator+(IOperand const &rhs) const;
+  IOperand const *operator-(IOperand const &rhs) const;
+  IOperand const *operator*(IOperand const &rhs) const;
+  IOperand const *operator/(IOperand const &rhs) const;
+  IOperand const *operator%(IOperand const &rhs) const;
+  std::string const &toString(void) const;
+  ~Operand(void) {}
 };
+
+// Example specialization:
+//
+// Operand<Int8, int8_t>;
+//
+// For creating Operands, a OperandFactory is needed
