631e6d3db7361707dab076744568f0965fd2c5c1
Soufiane Amini <samini@e3r5p2.1337.ma>
2024-09-07 21:46:13 +0100

    change enums name to avoid name conflict with math.h

--- avm-arithmetic/checked_arithmetic.hpp
+++ avm-arithmetic/checked_arithmetic.hpp
@@ -7,31 +7,31 @@ static_assert(std::numeric_limits<float>::has_infinity,
 static_assert(std::numeric_limits<double>::has_infinity,
               "infinity is not supported on this platform!");
 
-enum class CheckedArithmeticResult { SUCCESS, OVERFLOW, UNDERFLOW, INVALID };
+enum class CheckedArithmeticResult { CA_SUCCESS, CA_OVERFLOW, CA_UNDERFLOW, CA_INVALID };
 
 namespace checked_arithmetic {
 template <class T> CheckedArithmeticResult checked_add(T *res, T a, T b) {
   static_assert(sizeof(T) == -1,
                 "checked_arithmetic was not implemented for a type.");
-  return CheckedArithmeticResult::INVALID;
+  return CheckedArithmeticResult::CA_INVALID;
 }
 
 template <class T> CheckedArithmeticResult checked_sub(T *res, T a, T b) {
   static_assert(sizeof(T) == -1,
                 "checked_arithmetic was not implemented for a type.");
-  return CheckedArithmeticResult::INVALID;
+  return CheckedArithmeticResult::CA_INVALID;
 }
 
 template <class T> CheckedArithmeticResult checked_mul(T *res, T a, T b) {
   static_assert(sizeof(T) == -1,
                 "checked_arithmetic was not implemented for a type.");
-  return CheckedArithmeticResult::INVALID;
+  return CheckedArithmeticResult::CA_INVALID;
 }
 
 template <class T> CheckedArithmeticResult checked_div(T *res, T a, T b) {
   static_assert(sizeof(T) == -1,
                 "checked_arithmetic was not implemented for a type.");
-  return CheckedArithmeticResult::INVALID;
+  return CheckedArithmeticResult::CA_INVALID;
 }
 } // namespace checked_arithmetic
 
@@ -41,99 +41,99 @@ checked_arithmetic::checked_add<int8_t>(int8_t *res, int8_t a, int8_t b) {
 
   if (psnip_safe_int8_add(res, a, b)) {
     if ((a < 0) == (b < 0))
-      return CheckedArithmeticResult::OVERFLOW;
+      return CheckedArithmeticResult::CA_OVERFLOW;
     else
-      return CheckedArithmeticResult::UNDERFLOW;
+      return CheckedArithmeticResult::CA_UNDERFLOW;
   }
-  return CheckedArithmeticResult::SUCCESS;
+  return CheckedArithmeticResult::CA_SUCCESS;
 }
 
 template <>
 inline CheckedArithmeticResult
 checked_arithmetic::checked_sub<int8_t>(int8_t *res, int8_t a, int8_t b) {
   if (psnip_safe_int8_sub(res, a, b))
-    return CheckedArithmeticResult::OVERFLOW;
-  return CheckedArithmeticResult::SUCCESS;
+    return CheckedArithmeticResult::CA_OVERFLOW;
+  return CheckedArithmeticResult::CA_SUCCESS;
 }
 
 template <>
 inline CheckedArithmeticResult
 checked_arithmetic::checked_mul<int8_t>(int8_t *res, int8_t a, int8_t b) {
   if (psnip_safe_int8_mul(res, a, b))
-    return CheckedArithmeticResult::OVERFLOW;
-  return CheckedArithmeticResult::SUCCESS;
+    return CheckedArithmeticResult::CA_OVERFLOW;
+  return CheckedArithmeticResult::CA_SUCCESS;
 }
 
 template <>
 inline CheckedArithmeticResult
 checked_arithmetic::checked_div<int8_t>(int8_t *res, int8_t a, int8_t b) {
   if (psnip_safe_int8_div(res, a, b))
-    return CheckedArithmeticResult::OVERFLOW;
-  return CheckedArithmeticResult::SUCCESS;
+    return CheckedArithmeticResult::CA_OVERFLOW;
+  return CheckedArithmeticResult::CA_SUCCESS;
 }
 
 template <>
 inline CheckedArithmeticResult
 checked_arithmetic::checked_add<int16_t>(int16_t *res, int16_t a, int16_t b) {
   if (psnip_safe_int16_add(res, a, b))
-    return CheckedArithmeticResult::OVERFLOW;
-  return CheckedArithmeticResult::SUCCESS;
+    return CheckedArithmeticResult::CA_OVERFLOW;
+  return CheckedArithmeticResult::CA_SUCCESS;
 }
 
 template <>
 inline CheckedArithmeticResult
 checked_arithmetic::checked_sub<int16_t>(int16_t *res, int16_t a, int16_t b) {
   if (psnip_safe_int16_sub(res, a, b))
-    return CheckedArithmeticResult::OVERFLOW;
-  return CheckedArithmeticResult::SUCCESS;
+    return CheckedArithmeticResult::CA_OVERFLOW;
+  return CheckedArithmeticResult::CA_SUCCESS;
 }
 
 template <>
 inline CheckedArithmeticResult
 checked_arithmetic::checked_mul<int16_t>(int16_t *res, int16_t a, int16_t b) {
   if (psnip_safe_int16_mul(res, a, b))
-    return CheckedArithmeticResult::OVERFLOW;
-  return CheckedArithmeticResult::SUCCESS;
+    return CheckedArithmeticResult::CA_OVERFLOW;
+  return CheckedArithmeticResult::CA_SUCCESS;
 }
 
 template <>
 inline CheckedArithmeticResult
 checked_arithmetic::checked_div<int16_t>(int16_t *res, int16_t a, int16_t b) {
   if (psnip_safe_int16_div(res, a, b))
-    return CheckedArithmeticResult::OVERFLOW;
-  return CheckedArithmeticResult::SUCCESS;
+    return CheckedArithmeticResult::CA_OVERFLOW;
+  return CheckedArithmeticResult::CA_SUCCESS;
 }
 
 template <>
 inline CheckedArithmeticResult
 checked_arithmetic::checked_add<int32_t>(int32_t *res, int32_t a, int32_t b) {
   if (psnip_safe_int32_add(res, a, b))
-    return CheckedArithmeticResult::OVERFLOW;
-  return CheckedArithmeticResult::SUCCESS;
+    return CheckedArithmeticResult::CA_OVERFLOW;
+  return CheckedArithmeticResult::CA_SUCCESS;
 }
 
 template <>
 inline CheckedArithmeticResult
 checked_arithmetic::checked_sub<int32_t>(int32_t *res, int32_t a, int32_t b) {
   if (psnip_safe_int32_sub(res, a, b))
-    return CheckedArithmeticResult::OVERFLOW;
-  return CheckedArithmeticResult::SUCCESS;
+    return CheckedArithmeticResult::CA_OVERFLOW;
+  return CheckedArithmeticResult::CA_SUCCESS;
 }
 
 template <>
 inline CheckedArithmeticResult
 checked_arithmetic::checked_mul<int32_t>(int32_t *res, int32_t a, int32_t b) {
   if (psnip_safe_int32_mul(res, a, b))
-    return CheckedArithmeticResult::OVERFLOW;
-  return CheckedArithmeticResult::SUCCESS;
+    return CheckedArithmeticResult::CA_OVERFLOW;
+  return CheckedArithmeticResult::CA_SUCCESS;
 }
 
 template <>
 inline CheckedArithmeticResult
 checked_arithmetic::checked_div<int32_t>(int32_t *res, int32_t a, int32_t b) {
   if (psnip_safe_int32_div(res, a, b))
-    return CheckedArithmeticResult::OVERFLOW;
-  return CheckedArithmeticResult::SUCCESS;
+    return CheckedArithmeticResult::CA_OVERFLOW;
+  return CheckedArithmeticResult::CA_SUCCESS;
 }
 
 template <>
@@ -141,10 +141,10 @@ inline CheckedArithmeticResult
 checked_arithmetic::checked_add<float>(float *res, float a, float b) {
   *res = a + b;
   if (*res == std::numeric_limits<float>::infinity())
-    return CheckedArithmeticResult::OVERFLOW;
+    return CheckedArithmeticResult::CA_OVERFLOW;
   if (*res == -std::numeric_limits<float>::infinity())
-    return CheckedArithmeticResult::UNDERFLOW;
-  return CheckedArithmeticResult::SUCCESS;
+    return CheckedArithmeticResult::CA_UNDERFLOW;
+  return CheckedArithmeticResult::CA_SUCCESS;
 }
 
 template <>
@@ -152,10 +152,10 @@ inline CheckedArithmeticResult
 checked_arithmetic::checked_sub<float>(float *res, float a, float b) {
   *res = a - b;
   if (*res == std::numeric_limits<float>::infinity())
-    return CheckedArithmeticResult::OVERFLOW;
+    return CheckedArithmeticResult::CA_OVERFLOW;
   if (*res == -std::numeric_limits<float>::infinity())
-    return CheckedArithmeticResult::UNDERFLOW;
-  return CheckedArithmeticResult::SUCCESS;
+    return CheckedArithmeticResult::CA_UNDERFLOW;
+  return CheckedArithmeticResult::CA_SUCCESS;
 }
 
 template <>
@@ -163,10 +163,10 @@ inline CheckedArithmeticResult
 checked_arithmetic::checked_mul<float>(float *res, float a, float b) {
   *res = a * b;
   if (*res == std::numeric_limits<float>::infinity())
-    return CheckedArithmeticResult::OVERFLOW;
+    return CheckedArithmeticResult::CA_OVERFLOW;
   if (*res == -std::numeric_limits<float>::infinity())
-    return CheckedArithmeticResult::UNDERFLOW;
-  return CheckedArithmeticResult::SUCCESS;
+    return CheckedArithmeticResult::CA_UNDERFLOW;
+  return CheckedArithmeticResult::CA_SUCCESS;
 }
 
 template <>
@@ -174,10 +174,10 @@ inline CheckedArithmeticResult
 checked_arithmetic::checked_div<float>(float *res, float a, float b) {
   *res = a / b;
   if (*res == std::numeric_limits<float>::infinity())
-    return CheckedArithmeticResult::OVERFLOW;
+    return CheckedArithmeticResult::CA_OVERFLOW;
   if (*res == -std::numeric_limits<float>::infinity())
-    return CheckedArithmeticResult::UNDERFLOW;
-  return CheckedArithmeticResult::SUCCESS;
+    return CheckedArithmeticResult::CA_UNDERFLOW;
+  return CheckedArithmeticResult::CA_SUCCESS;
 }
 
 template <>
@@ -185,10 +185,10 @@ inline CheckedArithmeticResult
 checked_arithmetic::checked_add<double>(double *res, double a, double b) {
   *res = a + b;
   if (*res == std::numeric_limits<double>::infinity())
-    return CheckedArithmeticResult::OVERFLOW;
+    return CheckedArithmeticResult::CA_OVERFLOW;
   if (*res == -std::numeric_limits<double>::infinity())
-    return CheckedArithmeticResult::UNDERFLOW;
-  return CheckedArithmeticResult::SUCCESS;
+    return CheckedArithmeticResult::CA_UNDERFLOW;
+  return CheckedArithmeticResult::CA_SUCCESS;
 }
 
 template <>
@@ -196,10 +196,10 @@ inline CheckedArithmeticResult
 checked_arithmetic::checked_sub<double>(double *res, double a, double b) {
   *res = a - b;
   if (*res == std::numeric_limits<double>::infinity())
-    return CheckedArithmeticResult::OVERFLOW;
+    return CheckedArithmeticResult::CA_OVERFLOW;
   if (*res == -std::numeric_limits<double>::infinity())
-    return CheckedArithmeticResult::UNDERFLOW;
-  return CheckedArithmeticResult::SUCCESS;
+    return CheckedArithmeticResult::CA_UNDERFLOW;
+  return CheckedArithmeticResult::CA_SUCCESS;
 }
 
 template <>
@@ -207,10 +207,10 @@ inline CheckedArithmeticResult
 checked_arithmetic::checked_mul<double>(double *res, double a, double b) {
   *res = a * b;
   if (*res == std::numeric_limits<double>::infinity())
-    return CheckedArithmeticResult::OVERFLOW;
+    return CheckedArithmeticResult::CA_OVERFLOW;
   if (*res == -std::numeric_limits<double>::infinity())
-    return CheckedArithmeticResult::UNDERFLOW;
-  return CheckedArithmeticResult::SUCCESS;
+    return CheckedArithmeticResult::CA_UNDERFLOW;
+  return CheckedArithmeticResult::CA_SUCCESS;
 }
 
 template <>
@@ -218,8 +218,8 @@ inline CheckedArithmeticResult
 checked_arithmetic::checked_div<double>(double *res, double a, double b) {
   *res = a / b;
   if (*res == std::numeric_limits<double>::infinity())
-    return CheckedArithmeticResult::OVERFLOW;
+    return CheckedArithmeticResult::CA_OVERFLOW;
   if (*res == -std::numeric_limits<double>::infinity())
-    return CheckedArithmeticResult::UNDERFLOW;
-  return CheckedArithmeticResult::SUCCESS;
+    return CheckedArithmeticResult::CA_UNDERFLOW;
+  return CheckedArithmeticResult::CA_SUCCESS;
 }
