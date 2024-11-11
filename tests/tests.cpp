81cb53df633d34f8c71c469321a352ea6fd22b4a
samini <soufianeamini2002@gmail.com>
2024-11-11 11:29:36 +0100

    add test isolation by running them in processes

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,7 +1,27 @@
 #include "../lexer/Lexer.hpp"
+#include <cstdlib>
 #include <gtest/gtest.h>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define ISOLATE_TEST()                                                         \
+  pid_t pid = fork();                                                          \
+  if (pid == -1) {                                                             \
+    ASSERT_EQ(1, 2) << "Unable to create a process for the test";              \
+    return;                                                                    \
+  } else if (pid != 0) {                                                       \
+    int status;                                                                \
+    waitpid(pid, &status, 0);                                                  \
+    ASSERT_EQ(WEXITSTATUS(status), 0) << "Test didn't exit successfully";      \
+    ASSERT_EQ(WTERMSIG(status), 0) << "The program most likely segfaulted.";   \
+    ASSERT_EQ(WIFSIGNALED(status), false) << "A signal was not caught";        \
+    return;                                                                    \
+  }
 
 TEST(Lexer, SimpleTest) {
+  ISOLATE_TEST();
   Lexer lexer;
 
   auto tokens = lexer.lex("push int8(32)");
@@ -19,6 +39,22 @@ TEST(Lexer, SimpleTest) {
 
     ASSERT_EQ(a.type, b.type);
     ASSERT_EQ(a.literal, b.literal);
-		ASSERT_EQ(a.line, b.line);
+    ASSERT_EQ(a.line, b.line);
   }
+  exit(EXIT_SUCCESS);
 }
+
+TEST(Lexer, Comment) {
+  ISOLATE_TEST();
+  std::cout << "Inside thread\n";
+  Lexer lexer;
+
+  std::cout << "Before calling lexer" << std::endl;
+  auto tokens = lexer.lex("; Hello, this should be completely ignored");
+  std::cout << "After calling lexer" << std::endl;
+
+  ASSERT_EQ(tokens.size(), (unsigned long)0);
+  exit(EXIT_SUCCESS);
+}
+
+#undef ISOLATE_TEST
