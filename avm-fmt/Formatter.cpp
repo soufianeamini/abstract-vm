8579dfccf97c32aca9c7eb0ddba0988325578f5c
samini <soufianeamini2002@gmail.com>
2024-11-18 17:51:02 +0100

    add skeleton for formatter

--- avm-fmt/Formatter.cpp
+++ avm-fmt/Formatter.cpp
@@ -1,2 +1,27 @@
 #include "Formatter.hpp"
+#include "../avm-lexer/Lexer.hpp"
+#include <optional>
 
+std::optional<std::string> Formatter::format_avm(const std::string &input) {
+
+  Lexer lexer;
+  std::vector<Token> tokens = lexer.lex(input);
+  // TODO: Parse it to verify that it's a valid string, and only return a valid
+  // string if it's valid, if not it's an error, return std::nullopt
+
+  std::vector<Token> deduplicatedTokens = removeDuplicateSep(tokens);
+  std::string formatted_string = emitStringFromTokens(deduplicatedTokens);
+
+  return formatted_string;
+}
+
+std::vector<Token>
+Formatter::removeDuplicateSep(const std::vector<Token> &tokens) {
+  // TODO: remove duplicate SEP tokens, however other whitespaces is already
+  // removed from the lexing step
+  // NOTE: If there are more than 2 sep tokens, reduce them to 2 instead. But if
+  // there are 2 or 1 sep token just leave them as is
+  std::vector<Token> new_tokens;
+
+  return new_tokens;
+}
--- avm-fmt/Formatter.hpp
+++ avm-fmt/Formatter.hpp
@@ -1,7 +1,14 @@
 #pragma once
+#include "../avm-lexer/Token.hpp"
+#include <optional>
 #include <string>
+#include <vector>
 
 class Formatter {
+private:
+  std::vector<Token> removeDuplicateSep(const std::vector<Token> &tokens);
+	std::string emitStringFromTokens(const std::vector<Token> &tokens);
+
 public:
-  std::string format_avm(const std::string &input);
+  std::optional<std::string> format_avm(const std::string &input);
 };
