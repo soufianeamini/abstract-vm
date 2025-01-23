677ec1ff8706bad2041625597d8068c101612b9b
samini <soufianeamini2002@gmail.com>
2025-01-23 22:18:13 +0100

    input handler

--- avm-lib/InputHandler.cpp
+++ avm-lib/InputHandler.cpp
@@ -5,16 +5,14 @@
 #include <optional>
 #include <sstream>
 
-// TODO: Transform into class to add history handling later
-
 std::string InputHandler::readFile(const std::string &filename) {
-  std::fstream file(filename);
+  std::fstream file{filename};
 
   if (!file) {
     throw std::runtime_error("invalid file name: " + filename);
   }
 
-  std::stringstream ss;
+  std::stringstream ss{};
 
   ss << file.rdbuf();
   std::string content(ss.str());
@@ -23,9 +21,13 @@ std::string InputHandler::readFile(const std::string &filename) {
 }
 
 std::optional<std::string> InputHandler::readLine() {
-  std::string line;
+  std::string line{};
 
+	// TODO: Use something that allows you to capture arrow keys
   if (std::getline(std::cin, line)) {
+    if (!line.empty()) {
+      history.push_back(line);
+    }
     return line;
   }
 
--- avm-lib/InputHandler.hpp
+++ avm-lib/InputHandler.hpp
@@ -2,8 +2,14 @@
 
 #include <optional>
 #include <string>
+#include <vector>
 class InputHandler {
+private:
+  std::vector<std::string> history{};
+
 public:
   static std::string readFile(const std::string &filename);
   std::optional<std::string> readLine();
+  // TODO: add constructors
+  ~InputHandler();
 };
